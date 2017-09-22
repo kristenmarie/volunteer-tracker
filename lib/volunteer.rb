06f35077c41f5ffa05f4d802b291a4c77ec1396d
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-09-22 09:23:08 -0700

    add method to add created volunteer to database

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -23,5 +23,10 @@ class Volunteer
     volunteers
   end
 
+  def save
+    result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id} ) RETURNING id;")
+    @id = result.first.fetch("id").to_i
+  end
+
 
 end
--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -28,24 +28,23 @@ describe Volunteer do
       expect(Volunteer.all).to eq []
     end
   end
-end
 
-  #   it 'returns all volunteers' do
-  #     volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
-  #     volunteer1.save
-  #     volunteer2 = Volunteer.new({:name => 'Joe', :project_id => 1, :id => nil})
-  #     volunteer2.save
-  #     expect(Volunteer.all).to eq [volunteer1, volunteer2]
-  #   end
-  # end
-#
-  # describe '#save' do
-  #   it 'adds a volunteer to the database' do
-  #     volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
-  #     volunteer1.save
-  #     expect(Volunteer.all).to eq [volunteer1]
-  #   end
-  # end
+    it 'returns all volunteers' do
+      volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+      volunteer1.save
+      volunteer2 = Volunteer.new({:name => 'Joe', :project_id => 1, :id => nil})
+      volunteer2.save
+      expect(Volunteer.all).to eq [volunteer1, volunteer2]
+    end
+  end
+
+  describe '#save' do
+    it 'adds a volunteer to the database' do
+      volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+      volunteer1.save
+      expect(Volunteer.all).to eq [volunteer1]
+    end
+  end
 #
 #   describe '.find' do
 #     it 'returns a volunteer by id' do
