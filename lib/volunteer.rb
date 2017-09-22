415a460381500a611ca19a3b36dd4f24445fa59a
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-09-22 09:11:16 -0700

    add method to check if volunteers are the same using name

---
+++ lib/volunteer.rb
@@ -0,0 +1,15 @@
+class Volunteer
+  attr_accessor(:name, :id, :project_id)
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
+    @project_id = attributes.fetch(:project_id)
+  end
+
+  def ==(other_volunteer)
+    self.name.==(other_volunteer.name)
+  end
+
+
+end
